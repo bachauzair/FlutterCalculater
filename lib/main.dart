742f3b046f11e4b49ce05447d425ee16018f38ad
Uzair Bacha <bachauzair23@gmail.com>
2024-04-22 20:34:37 +0500

    Task 01

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+import 'CalculateControllerProvider.dart';
+import 'main.dart';
+import 'ThemeControllerProvider.dart';
+import 'Main Screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<CalculateControllerProvider>(
+          create: (_) => CalculateControllerProvider(),
+        ),
+        ChangeNotifierProvider<ThemeControllerProvider>(
+          create: (_) => ThemeControllerProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Flutter Calculator",
+        home: MainScreen(),
+      ),
+    );
+  }
+}
